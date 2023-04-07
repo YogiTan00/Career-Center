ebe705fcbc4193a2821a4b65eadcdfe06d127f7a
YogiTan00 <yogiwijaya00@gmail.com>
2023-04-07 17:57:34 +0700

    Feat: Update Work Experience - Validation id

--- internal/usecase/profile/update_work_experience.go
+++ internal/usecase/profile/update_work_experience.go
@@ -3,15 +3,21 @@ package profile
 import (
 	"CareerCenter/domain/entity/profile"
 	"context"
+	"github.com/google/uuid"
 )
 
 func (u UseCaseProfileInteractor) UpdateWorkExperience(ctx context.Context, id string, workExp *profile.WorkExperienceDTO) error {
+	check, err := uuid.Parse(id)
+	if err != nil {
+		return err
+	}
+
 	payload, err := profile.NewWorkExperience(workExp)
 	if err != nil {
 		return err
 	}
 
-	err = u.repoProfile.UpdateWorkExperience(ctx, id, payload)
+	err = u.repoProfile.UpdateWorkExperience(ctx, check.String(), payload)
 	if err != nil {
 		return err
 	}
