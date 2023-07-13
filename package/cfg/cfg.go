9bbf0e74ea3a7cf81091116ae38732f26f60b692
Taupik Pirdian <taupik.pirdian@salt.co.id>
2023-07-13 23:08:33 +0700

    env implement

---
+++ package/cfg/cfg.go
@@ -0,0 +1,19 @@
+package cfg
+
+type Config struct {
+	DEBUG                string
+	TIMEOUT              string
+	ADDRESS              string
+	DB_HOST              string
+	DB_PORT              string
+	DB_USER              string
+	DB_PASS              string
+	DB_NAME              string
+	PATH_IMAGE_UPLOAD    string
+	PATH_FILE_UPLOAD     string
+	CONFIG_SMTP_HOST     string
+	CONFIG_SMTP_PORT     string
+	CONFIG_SENDER_NAME   string
+	CONFIG_AUTH_EMAIL    string
+	CONFIG_AUTH_PASSWORD string
+}
