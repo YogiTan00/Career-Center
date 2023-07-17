1f13ef3f25f971cd8a7114cd01f0ddd19857c916
YogiTan00 <yogiwijaya00@gmail.com>
2023-07-17 21:14:48 +0700

    Fix: Fixing Cant Register

--- internal/repository/account/create_account.go
+++ internal/repository/account/create_account.go
@@ -4,15 +4,16 @@ import (
 	"CareerCenter/domain/entity/account"
 	"CareerCenter/internal/repository/mapper"
 	"CareerCenter/internal/repository/models"
-	"CareerCenter/utils/exceptions"
 	"context"
+	"errors"
+	"github.com/go-sql-driver/mysql"
 	"github.com/rocketlaunchr/dbq/v2"
 	"time"
 )
 
 func (r AccountMysqlInteractor) CreateAccount(ctx context.Context, data *account.Account) error {
 	var (
-		checkErr bool
+		checkErr error
 	)
 	ctx, cancel := context.WithTimeout(ctx, 60*time.Second)
 	defer cancel()
@@ -25,14 +26,17 @@ func (r AccountMysqlInteractor) CreateAccount(ctx context.Context, data *account
 		_, errStore := E(ctx, stmt, nil, postModelStruct)
 
 		if errStore != nil {
-			checkErr = true
-		}
+			if mysqlErr, ok := errStore.(*mysql.MySQLError); ok && mysqlErr.Number == 1062 {
+				checkErr = errors.New("e-mail has been registered")
+			} else {
+				checkErr = errStore
+			}
 
+		}
 		_ = txCommit()
 	})
-
-	if checkErr == true {
-		return exceptions.ErrCustomString("e-mail has been registered")
+	if checkErr != nil {
+		return checkErr
 	}
 
 	return err
