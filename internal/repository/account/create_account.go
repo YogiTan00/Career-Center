678fcec1a1a6f23541013d86f0d894d9d25f3ad4
Taupik Pirdian <taupik.pirdian@salt.co.id>
2023-07-18 17:12:55 +0700

    fix: issue register deleted_at

--- internal/repository/account/create_account.go
+++ internal/repository/account/create_account.go
@@ -6,9 +6,10 @@ import (
 	"CareerCenter/internal/repository/models"
 	"context"
 	"errors"
+	"time"
+
 	"github.com/go-sql-driver/mysql"
 	"github.com/rocketlaunchr/dbq/v2"
-	"time"
 )
 
 func (r AccountMysqlInteractor) CreateAccount(ctx context.Context, data *account.Account) error {
@@ -20,7 +21,6 @@ func (r AccountMysqlInteractor) CreateAccount(ctx context.Context, data *account
 
 	err := dbq.Tx(ctx, r.DbConn, func(tx interface{}, Q dbq.QFn, E dbq.EFn, txCommit dbq.TxCommit) {
 		postModelStruct := mapper.DomainToInterface(data)
-
 		stmt := dbq.INSERTStmt(models.GetTableNameAccount(), models.TableAccount(), len(postModelStruct), dbq.MySQL)
 
 		_, errStore := E(ctx, stmt, nil, postModelStruct)
